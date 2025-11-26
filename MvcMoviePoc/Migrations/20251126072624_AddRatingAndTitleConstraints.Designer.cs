db8e05fb3890aea214f565e6079c2a79e6853b48
baki504 <baki504@msn.com>
2025-11-26 18:11:53 +0900

    update database

--- MvcMoviePoc/Migrations/20251126072624_AddRatingAndTitleConstraints.Designer.cs
+++ MvcMoviePoc/Migrations/20251126072624_AddRatingAndTitleConstraints.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMoviePoc.Data;
 
@@ -11,9 +12,11 @@ using MvcMoviePoc.Data;
 namespace MvcMoviePoc.Migrations
 {
     [DbContext(typeof(MvcMoviePocContext))]
-    partial class MvcMoviePocContextModelSnapshot : ModelSnapshot
+    [Migration("20251126072624_AddRatingAndTitleConstraints")]
+    partial class AddRatingAndTitleConstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
